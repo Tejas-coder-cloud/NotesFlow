78a3089585303198bdc3df626632c23a8478ae03
Tejas-coder-cloud <dhatraktejas07@gmail.com>
2026-06-20 09:49:23 +0530

    Added the toast for invalid credentials

--- frontend/front-end/src/pages/Login.jsx
+++ frontend/front-end/src/pages/Login.jsx
@@ -26,70 +26,75 @@ function Login() {
       );
       navigate("/dashboard");
       console.log(response.data);
-      toast.success("Login Successful");
+      toast.success( `Welcome back ${user.name}!`);
     } catch (error) {
-
-      console.log(error.response.data);
-
-      alert(error.response.data.message);
+      if (error.response?.status === 401) {
+        toast.error(
+          "Invalid Email or Password"
+        );
+      } else {
+        toast.error(
+          "Something went wrong"
+        );
+      }
     }
   };
   return (
     <div className="login-page">
-    <div className="login-container">
-      <div className="login-card">
-        <h1 className="login-title">
-          Welcome Back
-        </h1>
-        <form onSubmit={handleSubmit}>
-          <input
-            className="login-input"
-            type="email"
-            placeholder="Enter Email"
-            value={email}
-            onChange={(e) =>
-              setEmail(e.target.value)
-            }
-          />
-          <div className="password-container">
+      <div className="login-container">
+        <div className="login-card">
+          <h1 className="login-title">
+            Welcome Back
+          </h1>
+          <form onSubmit={handleSubmit}>
             <input
               className="login-input"
-              type={showPassword ? "text" : "password"}
-              placeholder="Enter Password"
-              value={password}
+              type="email"
+              placeholder="Enter Email"
+              value={email}
               onChange={(e) =>
-                setPassword(e.target.value)
+                setEmail(e.target.value)
               }
             />
-            <span
-              className="eye-icon"
-              onClick={() =>
-                setShowPassword(!showPassword)
-              }
+            <div className="password-container">
+              <input
+                className="login-input"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) =>
+                  setPassword(e.target.value)
+                }
+              />
+              <span
+                className="eye-icon"
+                onClick={() =>
+                  setShowPassword(!showPassword)
+                }
+              >
+                {
+                  showPassword
+                    ? <FaEyeSlash />
+                    : <FaEye />
+                }
+              </span>
+            </div>
+            <button
+              className="login-btn"
+              type="submit"
             >
-              {
-                showPassword
-                  ? <FaEyeSlash />
-                  : <FaEye />
-              }
-            </span>
+              Login
+            </button>
+          </form>
+          <div className="register-link">
+            Don't have an account?
+            <br />
+            <Link to="/register">
+              Register Here
+            </Link>
           </div>
-          <button
-            className="login-btn"
-            type="submit"
-          >
-            Login
-          </button>
-        </form>
-        <div className="register-link">
-          Don't have an account?
-          <br />
-          <Link to="/register">
-            Register Here
-          </Link>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- frontend/front-end/src/pages/Register.jsx
+++ frontend/front-end/src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 import axios from "axios";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "../styles/Register.css";
@@ -20,80 +21,88 @@ function Register() {
                 }
             );
             console.log(response.data);
-            alert("User registered successfully");
+            toast.success(
+                "User registered successfully"
+            );
         } catch (error) {
             console.log(error);
             if (error.response) {
-                alert(error.response.data.message);
+                toast.error(
+                    error.response.data.message
+                );
+
             } else {
-                alert("Backend Server Not Running");
+                   toast.error(
+        "Backend Server Not Running"
+    );
+
             }
         }
     };
     return (
         <div className="register-page">
-        <div className="register-container">
-            <div className="register-card">
-                <h1 className="register-title">
-                    NotesFlow
-                </h1>
-                <p>
-                    Create your account
-                </p>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        className="register-input"
-                        type="text"
-                        placeholder="Enter Name"
-                        value={name}
-                        onChange={(e) =>
-                            setName(e.target.value)
-                        }
-                    />
-                    <input
-                        className="register-input"
-                        type="email"
-                        placeholder="Enter Email"
-                        value={email}
-                        onChange={(e) =>
-                            setEmail(e.target.value)
-                        }
-                    />
-                    <div className="password-container">
+            <div className="register-container">
+                <div className="register-card">
+                    <h1 className="register-title">
+                        NotesFlow
+                    </h1>
+                    <p>
+                        Create your account
+                    </p>
+                    <form onSubmit={handleSubmit}>
                         <input
                             className="register-input"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Enter Password"
-                            value={password}
+                            type="text"
+                            placeholder="Enter Name"
+                            value={name}
                             onChange={(e) =>
-                                setPassword(e.target.value)
+                                setName(e.target.value)
                             }
                         />
-                        <span
-                            className="eye-icon"
-                            onClick={() =>
-                                setShowPassword(!showPassword)
+                        <input
+                            className="register-input"
+                            type="email"
+                            placeholder="Enter Email"
+                            value={email}
+                            onChange={(e) =>
+                                setEmail(e.target.value)
                             }
+                        />
+                        <div className="password-container">
+                            <input
+                                className="register-input"
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Enter Password"
+                                value={password}
+                                onChange={(e) =>
+                                    setPassword(e.target.value)
+                                }
+                            />
+                            <span
+                                className="eye-icon"
+                                onClick={() =>
+                                    setShowPassword(!showPassword)
+                                }
+                            >
+                                {showPassword ? <FaEyeSlash /> : <FaEye />}
+                            </span>
+                        </div>
+                        <button
+                            className="register-btn"
+                            type="submit"
                         >
-                        {showPassword ? <FaEyeSlash /> : <FaEye />}
-                        </span>
+                            Register
+                        </button>
+                    </form>
+                    <div className="login-link">
+                        Already have an account?
+                        <br />
+                        <Link to="/">
+                            Login Here
+                        </Link>
                     </div>
-                    <button
-                        className="register-btn"
-                        type="submit"
-                    >
-                        Register
-                    </button>
-                </form>
-                <div className="login-link">
-                    Already have an account?
-                    <br />
-                    <Link to="/">
-                        Login Here
-                    </Link>
                 </div>
             </div>
-        </div>
         </div>
     );
 }
